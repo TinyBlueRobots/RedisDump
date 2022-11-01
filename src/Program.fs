988b37d1043b11f97b1227b8b1e151c56a459300
Jon Canning <jon.canning@gmail.com>
2022-11-01 12:11:54 +0000

    update to net6

--- src/Program.fs
+++ src/Program.fs
@@ -4,10 +4,10 @@ open System.IO
 open FSharp.Control
 
 type Arguments =
-| ConnectionString of string
-| Database of int
-| Output of string
-with
+  | ConnectionString of string
+  | Database of int
+  | Output of string
+
   interface IArgParserTemplate with
     member __.Usage =
       match __ with
@@ -15,13 +15,13 @@ with
       | Database _ -> "database number (defaults to 0)"
       | Output _ -> "output directory (defaults to currentDirectory/database)"
 
-let stringGetAsync (database : IDatabase) (key : RedisKey) =
+let stringGetAsync (database: IDatabase) (key: RedisKey) =
   async {
     let! redisValue = database.StringGetAsync key |> Async.AwaitTask
     return string key, string redisValue
   }
 
-let writeAllText (directory : DirectoryInfo) (name, text) =
+let writeAllText (directory: DirectoryInfo) (name, text) =
   async {
     let path = Path.Combine(directory.FullName, name)
     return! File.WriteAllTextAsync(path, text) |> Async.AwaitTask
@@ -30,28 +30,35 @@ let writeAllText (directory : DirectoryInfo) (name, text) =
 [<EntryPoint>]
 let main argv =
   let parser = ArgumentParser.Create<Arguments>()
+
   try
     let results = parser.Parse argv
-    let connectionString = results.GetResult(<@ ConnectionString  @>, "localhost")
+    let connectionString = results.GetResult(<@ ConnectionString @>, "localhost")
     let database = results.GetResult(<@ Database @>, 0)
     let connectionMultiplexer = ConnectionMultiplexer.Connect connectionString
-    let output = 
+
+    let output =
       match results.TryGetResult(<@ Output @>) with
       | Some output -> output
-      | _ -> Path.Combine(Directory.GetCurrentDirectory(), string database) 
+      | _ -> Path.Combine(Directory.GetCurrentDirectory(), string database)
       |> Directory.CreateDirectory
-    let server = connectionMultiplexer.GetEndPoints().[0] |> connectionMultiplexer.GetServer
+
+    let server =
+      connectionMultiplexer.GetEndPoints().[0] |> connectionMultiplexer.GetServer
+
     let keys = server.Keys database
     let redisDatabase = connectionMultiplexer.GetDatabase database
+
     async {
       do!
         keys
         |> AsyncSeq.ofSeq
         |> AsyncSeq.mapAsyncParallel (stringGetAsync redisDatabase)
         |> AsyncSeq.iterAsyncParallel (writeAllText output)
-    } 
+    }
     |> Async.RunSynchronously
   with
   | :? ArguParseException as ex -> printfn "%s" ex.Message
   | ex -> raise ex
+
   0
